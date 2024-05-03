d450527de8572f7927ea192236451c63c64f3608
Paul Taylor <neon.taylor@gmail.com>
2024-05-03 15:31:39 -0400

    ubuntu build: initial files

---
+++ ubuntu_build/README.txt
@@ -0,0 +1,37 @@
+
+Notes on installing the Docker engine on Ubuntu are here:
+https://docs.docker.com/engine/install/ubuntu/
+Specifically, PT is going to follow this route for installing:
+https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+
+This example setup has been modeling on Vinai Roopchansingh's from here:
+https://github.com/roopchansinghv/dockerImage4DevMR
+
+To build the image, check out options/notes from here:
+https://docs.docker.com/get-started/02_our_app/
+You can run:
+
+  docker build -t afni_doc_v1 .
+
+where:
++ the '.' means to find a file called Dockerfile in the current
+  directory
++ '-t ..' makes a tag for the created image, which will be effectively
+  the name for referencing/using the created image. This can have more
+  functionality, see https://docs.docker.com/reference/cli/docker/image/tag/
+
+---------------------------------------------------------------------------
+
+NOTES
+
+When trying some docker commands, I kept getting this error message:
+  permission denied while trying to connect to the Docker daemon socket at unix:///var/run/docker.sock: Get "http://%2Fvar%2Frun%2Fdocker.sock/v1.45/containers/json": dial unix /var/run/docker.sock: connect: permission denied
+
+From here (https://stackoverflow.com/questions/47854463/docker-got-permission-denied-while-trying-to-connect-to-the-docker-daemon-socke), I then ran:
+  
+  sudo usermod -a -G docker $USER
+
+... and then logged out and back in, and I was able to run docker
+commands without an error, such as:
+
+  docker container ls -a 
---
+++ ubuntu_build/create_machine.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# This file is a duplicate of this one in the afni GitHub repo:
+#   src/other_builds/OS_notes.linux_ubuntu_24_64_a_admin.txt
+# NB: the 'sudo ..' part of each has been removed
+
+# add universe repo
+add-apt-repository universe -y
+
+# update package manager
+apt-get update
+
+# get main dependencies
+apt-get install -y      tcsh xfonts-base libssl-dev       \
+                        python-is-python3                 \
+                        python3-matplotlib python3-numpy  \
+                        python3-flask python3-flask-cors  \
+                        python3-pil                       \
+                        gsl-bin netpbm gnome-tweaks       \
+                        libjpeg62 xvfb xterm vim curl     \
+                        gedit evince eog                  \
+                        libglu1-mesa-dev libglw1-mesa-dev \
+                        libxm4 build-essential            \
+                        libcurl4-openssl-dev libxml2-dev  \
+                        libgfortran-14-dev libgomp1       \
+                        gnome-terminal nautilus           \
+                        firefox xfonts-100dpi             \
+                        r-base-dev cmake bc
+
+# get more dependencies for R-package brms
+apt-get install -y      libgdal-dev libopenblas-dev       \
+                        libnode-dev libudunits2-dev
+
+# make symlink for package GSL
+ln -s                                                     \
+     /usr/lib/x86_64-linux-gnu/libgsl.so.27               \
+     /usr/lib/x86_64-linux-gnu/libgsl.so.19
