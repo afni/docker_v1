526b02a3689153ba507f7f47913750c088f7a688
afni-discoraj <justin.rajendra@nih.gov>
2025-08-14 15:20:58 -0400

    added universal with some fixes and root script changes

---
+++ afni_docker_universal/create_machine_root.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## echo commands to terminal
+set -x
+
+######################################
+## update package manager and get some required packages
+apt-get update
+apt install -y software-properties-common
+add-apt-repository universe -y
+apt-get update
+apt-get install -y curl tcsh sudo ffmpeg
+
+######################################
+## get the admin script for the afni install
+curl -O https://raw.githubusercontent.com/afni/afni/master/src/other_builds/OS_notes.linux_ubuntu_24_64_a_admin.txt
+bash OS_notes.linux_ubuntu_24_64_a_admin.txt
+
+######################################
+## make folders and links for mounting local host drives and local syncs
+mkdir -p /usr/local/abin
+chown -R afni_user:afni_user /usr/local/abin
+mkdir -p /home/external/
+mkdir -p /home/afni_user/
+chown -R afni_user:afni_user /home/afni_user/
+chown -R afni_user:afni_user /home/external/
+
+## ( -h for symbolic link )
+ln -s /home/external/.afnirc /home/afni_user/.afnirc
+chown -h afni_user:afni_user /home/afni_user/.afnirc
