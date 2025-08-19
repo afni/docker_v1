6a2a82ecac42476b392c75c730a67c3d41c3c119
afni-discoraj <justin.rajendra@nih.gov>
2025-08-19 16:28:55 -0400

    firefox fix and temp uid fix

--- afni_docker_universal/create_machine_root.sh
+++ afni_docker_universal/create_machine_root.sh
@@ -9,13 +9,30 @@ apt-get update
 apt install -y software-properties-common
 add-apt-repository universe -y
 apt-get update
-apt-get install -y curl tcsh sudo ffmpeg iputils-ping
-snap install -y firefox
+apt-get install -y curl tcsh sudo ffmpeg iputils-ping wget
 
 ######################################
 ## get the admin script for the afni install
 curl -O https://raw.githubusercontent.com/afni/afni/master/src/other_builds/OS_notes.linux_ubuntu_24_64_a_admin.txt
 bash OS_notes.linux_ubuntu_24_64_a_admin.txt
+
+######################################
+## firefox complicated install because of snap
+apt-get remove -y firefox
+
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+
+sudo apt-get update && sudo apt-get install -y firefox
+
 
 ######################################
 ## make folders and links for mounting local host drives and local syncs
