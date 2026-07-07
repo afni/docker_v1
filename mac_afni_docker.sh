763cc26ed7f820ee634f7b3bc87045e7c1773bb3
afni-discoraj <justin.rajendra@nih.gov>
2026-07-07 10:36:08 -0400

    bash version of the mac launcher

---
+++ mac_afni_docker.sh
@@ -0,0 +1,238 @@
+#!/bin/bash
+
+#################################################
+## 06/2026 Justin Rajendra
+## launch the afni docker container on macOS
+## universal build
+
+## get the current program name
+prog="mac_afni_docker"
+
+#################################################
+## help function
+SHOW_HELP() {
+    cat << EOF
+
+   ----------------------------------------------------------------------------
+   $prog ~1~
+
+           Helper script to launch the afni docker on macOS.
+           This script does not need any arguments.  
+
+           REQUIREMENTS:  
+           1. This MUST be run on macOS.
+           2. Docker MUST be installed.
+           3. Xquartz MUST be installed.
+
+           This script will check for all of the above.
+           If Docker and/or Xquartz are not running, the script will
+           launch them for you.
+
+   -----------------------------------------------------------------------------
+   options: ~1~
+
+      -latest        : Pull a new afni docker image even if an older one exists.
+                       This will overwrite the previous local image with the 
+                       newest one from docker hub.  If the afni docker image 
+                       does not exist locally, the latest will be pulled.
+
+      -image [IMG]    : Launch a different docker image.  This can be a local 
+                        image or something from docker hub.  Please surround 
+                        text in double quotes " ".
+                        Default is "discoraj/afni_docker_universal:latest".
+        
+      -display [DISP] : Use a different display environment variable for 
+                        testing purposes.
+                        Please surround text in double quotes " ".
+                        Defaults (as of 06/2026) are:
+                        macOS: "host.docker.internal:0"
+                        Linux: DISPLAY environment variable.
+
+      -help        : Show this help.
+
+   -----------------------------------------------------------------------------
+   examples: ~1~
+
+      $prog
+                : Just launch the afni docker.  
+
+      $prog -latest
+                : Launch the afni docker and update the local image.
+
+      $prog -image "Public_Image_Ltd"
+                : Launch the docker with the image named "Public_Image_Ltd".
+                  This will look for the image locally first then pull it from 
+                  docker hub if it is not found.
+
+      $prog -help
+
+   -----------------------------------------------------------------------------
+   Justin Rajendra 06/2026
+
+EOF
+    exit 0
+}
+
+#################################################
+## get arguments or show help
+
+## default docker image
+dock_img="discoraj/afni_docker_universal:latest"
+pull="missing"
+disp="host.docker.internal:0"
+
+# Put all arguments into a bash array (0-indexed)
+argv=("$@")
+narg=0
+amax=$#
+
+while [[ $narg -lt $amax ]]; do
+    if [[ "${argv[$narg]}" == "-image" ]]; then
+        ((narg++))
+        dock_img="${argv[$narg]}"
+    elif [[ "${argv[$narg]}" == "-display" ]]; then
+        ((narg++))
+        disp="${argv[$narg]}"
+    elif [[ "${argv[$narg]}" == "-latest" ]]; then
+        pull="always"
+    elif [[ "${argv[$narg]}" == "-help" || "${argv[$narg]}" == "-h" ]]; then
+        SHOW_HELP
+    else
+        SHOW_HELP
+    fi
+    ((narg++))
+done
+
+################################################
+## check to see if things exist / are installed
+
+## check for macOS
+macOS=`sw_vers -productName`
+if [[ "$macOS" != "macOS" ]]; then
+    echo ; echo "** ERROR: You don't seem to be running macOS." ; echo
+    echo "If you are, there may be a problem with this program..."
+    echo "Please post the error to: https://discuss.afni.nimh.nih.gov" ; echo
+    exit 1
+fi
+
+## check for docker
+docker_bin=`command -v docker`
+if [[ -z "$docker_bin" ]]; then
+    echo ; echo "** ERROR: Docker not found." ; echo
+    echo "Is Docker installed?"
+    echo "If Docker is installed, can you open it?"
+    echo "If not, check your paths or manually launch it from Finder." ; echo
+    exit 1
+fi
+
+## check for XQuartz
+xquartz_bin=`command -v Xquartz || command -v XQuartz`
+if [[ -z "$xquartz_bin" ]]; then
+    ## Fallback in case it is installed but not in PATH
+    if [[ ! -d "/Applications/Utilities/XQuartz.app" && ! -d "/Applications/XQuartz.app" ]]; then
+        echo ; echo "** ERROR: XQuartz not found." ; echo
+        echo "Is XQuartz installed and in your PATH?"
+        echo "If not, you can install it from homebrew"
+        echo "or from here: https://www.xquartz.org" ; echo
+        exit 1
+    fi
+fi
+
+## check for Xquartz security setting
+xquartz_sec=`defaults read org.xquartz.X11.plist nolisten_tcp`
+xquartz_auth=`defaults read org.xquartz.X11 no_auth`
+
+if [[ "$xquartz_sec" == "1" || "$xquartz_auth" == "0" ]]; then
+    echo ; echo "** ERROR: XQuartz is blocking tcp clients needed for docker."
+    echo
+    echo "You can fix this by entering the following commands with XQuartz quit:"
+    echo "defaults write org.xquartz.X11.plist nolisten_tcp -bool false"
+    echo "defaults write org.xquartz.X11 no_auth -boolean true"
+    echo
+    echo "OR you can do this in the Security tab of the Settings in XQuartz."
+    echo "Uncheck the 'Authenticate connections' box."
+    echo "AND"
+    echo "Check the 'Allow connections from network clients' box."
+    echo
+    echo "OR I can fix this for you now."
+    echo "Do you want me to fix this? You only have to do this once."
+    echo "XQuartz will need to be quit to do this."
+    echo
+    
+    read -p "Enter Y to quit XQuartz and fix this or enter anything else to exit: " fix
+
+    if [[ "$fix" == "Y" ]]; then
+        xquart_pid=`pgrep -i Xquartz`
+        if [[ -n "$xquart_pid" ]]; then
+            echo "Killing all XQuartz..."
+            killall Xquartz
+            sleep 2
+        fi
+
+        echo
+        echo "defaults write org.xquartz.X11.plist nolisten_tcp -bool false"
+        defaults write org.xquartz.X11.plist nolisten_tcp -bool false
+        echo "defaults write org.xquartz.X11 no_auth -boolean true"
+        defaults write org.xquartz.X11 no_auth -bool true
+        echo
+    else
+        echo
+        echo "Please fix the XQuartz setting with the above instructions."
+        echo
+        exit 1
+    fi
+fi   ## end check for XQuartz security setting
+
+#########################################################
+## check to see if things are running.  If not, open them
+
+## check if docker is running and launch it if not
+## this file should exist if docker is running
+if [[ ! -e "/Users/${USER}/.docker/run/docker.sock" ]]; then
+    echo ; echo "+* Warning: Docker daemon is not running."
+    echo "Launching Docker daemon. Please wait."
+    open -a Docker
+    sleep 5 ; echo
+
+    while [[ ! -e "/Users/${USER}/.docker/run/docker.sock" ]]; do 
+        echo "Waiting for Docker daemon..."
+        sleep 2
+    done
+fi
+
+## check if XQuartz is running and launch it if not
+xquart_pid=`pgrep -i Xquartz`
+if [[ -z "$xquart_pid" ]]; then
+    echo ; echo "+* Warning: Xquartz is not running."
+    echo "Launching Xquartz. Please wait."
+    open -a XQuartz
+    sleep 5 ; echo
+
+    while true; do 
+        xquart_pid=`pgrep -i Xquartz`
+        if [[ -z "$xquart_pid" ]]; then
+            echo "Waiting for Xquartz..."
+            sleep 2
+        else
+            break
+        fi
+    done
+fi
+
+###########################################################################
+## run docker container
+
+## run the docker
+echo ; echo "Launching ${dock_img} ...beep boop beep boop..." ; echo
+docker run -ti --rm \
+       -u root \
+       -v "${HOME}:/home/external" \
+       -v /tmp/.X11-unix:/tmp/.X11-unix \
+       --env DISPLAY="${disp}" \
+       --env USERID="`id -u`" \
+       --env GRPID="`id -g`" \
+       --env GRPNAME="`id -gn`" \
+       --env USERNAME="`id -u -n`" \
+       --pull "${pull}" \
+       "${dock_img}"
+exit 0
